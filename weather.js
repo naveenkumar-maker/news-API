58adb15c98e9bc48dfaffff21629a870c46f39d9
naveenkumar-maker <65797041+naveenkumar-maker@users.noreply.github.com>
2020-06-01 16:37:56 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,30 +1,50 @@
-let technologyApi = "http://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=0f83bfae73324f299c0448ace93ff3a1";
-
-window.fetch(technologyApi)
-.then((techData) => {
-     techData.json()
-     .then((tech) => {
-         //console.log(tech.articles);
-          let techFeed = tech.articles;
-          let firstTechBlock = tech.articles;
-          let techNewsBlockOne = firstTechBlock[0];
-          document.getElementById("techFirstTemplate").innerHTML = `
-             <ul>
-                 <img src="${techNewsBlockOne.urlToImage}" alt="ImageOne">
-             </ul>
-          `;
-            let output = [];
-            for(let techOf of techFeed) {
-                output += `
-                <ul>
-                    <a href="${techOf.url}" target="_blank">
-                    <li>${techOf.title}</li>
-                    </a>
-                </ul>
-                `;
-             document.getElementById("techAllTemplate").innerHTML = output;
-         }
-     })
-     .catch((err) => console.log(err));
-})
-.catch((err) => console.log(err));
+async function getPlaces() {
+    var input = document.getElementById("input");
+    await new google.maps.places.Autocomplete(input);
+  }
+  let form = document.querySelector("form");
+  
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let searchKey = document.getElementById("input").value;
+    console.log(searchKey);
+    searchWeather(searchKey);
+  });
+  
+  function searchWeather(searchKey) {
+    let WeatherApi = `api.openweathermap.org/data/2.5/weather?q=${searchKey}&appid={4795c7432113fab383ba99f26dd99bc0
+    }`;
+  
+    window
+      .fetch(WeatherApi)
+      .then((data) => {
+        data
+          .json()
+          .then((weather) => {
+            console.log(weather);
+            let weatherData = weather.weather;
+            let main = weather.main;
+            output = [];
+            for (let x of weatherData) {
+              output += `
+                  <div class="col-md-12 mt-4 card">
+                  <div class="card-body">
+                  <h1>${weathers.name}</h1>
+                  <div>
+                  <p class="icon">
+                  <img src="http://openweathermap.org/img/wn/${x.icon}.png"/></p>
+                  <p><span>temp:</span>
+                  <span class="temp">${weathers.main.temp}&deg;c</span></p>
+                  <p class="float-left">humidity:${weathers.main.humidity}&deg;c</p>
+                  <p class="des float-left">${x.description}</p>
+                  <p class="des float-right">${x.min}</p></div>
+                  </div>
+                  </div>
+                  `;
+              document.getElementById("weatherTemplate").innerHTML = output;
+            }
+          })
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => console.log(err));
+  }
